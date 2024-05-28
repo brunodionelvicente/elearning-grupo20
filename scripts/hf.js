465ca761009ff1d14fd6969d8c62028188d42e9d
Alicia Aramayo <aramayoalicia24@gmail.com>
2024-05-28 18:21:55 -0300

    Inicio de sesion y registro simples
    se agrego favicon, imagen de logo, un js con fragment de header y footer

---
+++ scripts/hf.js
@@ -0,0 +1,26 @@
+cad=`<div>
+<a href="/">
+  <img id="logo" src="./assets/logo.png" alt="logo">
+</a>
+</div>
+<div class="search_container">
+<button class="search_icon-button">
+  <span class="material-symbols-outlined"> search </span>
+</button>
+<input type="text" class="search_input" placeholder="¿Qué te gustaría aprender?" />
+</div>
+<nav>
+<ul>
+  <li class=""><a href="" onclick="route()">Home</a> </li>
+  <li class=""><a href="">Registro</a> </li>
+  <li class=""><a href="formulario_contacto.html">Contacto</a>
+</ul>
+</nav>`
+document.querySelector("header").innerHTML = cad
+/*----------------------------------------------------------*/
+cad=`
+<div>
+<h3>🚀 Proyecto educativo Codo a Codo 2024 🚀</h3>
+</div>
+`
+document.querySelector("footer").innerHTML=cad
