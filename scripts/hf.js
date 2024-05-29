7ff71983de06a7da77d468bf903335bf12b901b8
Bruno Dionel <bruno.dionel@livocompany.com>
2024-05-29 00:52:01 -0300

    TAREA-FORMULARIO_CONTACTO - agrega header mobile

--- scripts/hf.js
+++ scripts/hf.js
@@ -1,5 +1,5 @@
 cad=`<div>
-<a href="/">
+<a href="/" class="logo_container">
   <img id="logo" src="./assets/logo.png" alt="logo">
 </a>
 </div>
@@ -9,13 +9,15 @@ cad=`<div>
 </button>
 <input type="text" class="search_input" placeholder="¿Qué te gustaría aprender?" />
 </div>
-<nav>
-<ul>
+<nav class="nav" id="nav">
+<ul class="nav-list">
   <li class=""><a href="iniciosesion.html">Login</a> </li>
   <li class=""><a href="registro.html">Registro</a> </li>
   <li class=""><a href="formulario_contacto.html">Contacto</a>
 </ul>
-</nav>`
+</nav>
+<button class="nav-toggle" id="nav-toggle">☰</button>
+`
 document.querySelector("header").innerHTML = cad
 /*----------------------------------------------------------*/
 cad=`
